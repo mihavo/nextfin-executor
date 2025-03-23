60beb41b82f356e8e65714a3d066d22ae1a41719
Michael Volakis <mivolakis@gmail.com>
2025-03-23 18:52:44 +0200

    initial commit

---
+++ src/main/java/com/nextfin/executor/NextfinExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.nextfin.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextfinExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextfinExecutorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nextfin/executor/NextfinExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextfin.executor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextfinExecutorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
