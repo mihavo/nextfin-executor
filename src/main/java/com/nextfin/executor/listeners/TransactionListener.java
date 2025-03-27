4308062e40c229b01acf65abb4a4edc9636553e7
Michael Volakis <mivolakis@gmail.com>
2025-03-27 18:20:09 +0200

    feat: listener -> executor call

--- src/main/java/com/nextfin/executor/listeners/TransactionListener.java
+++ src/main/java/com/nextfin/executor/listeners/TransactionListener.java
@@ -1,17 +1,22 @@
 package com.nextfin.executor.listeners;
 
+import com.nextfin.executor.service.ExecutorService;
 import com.nextfin.transaction.entity.Transaction;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class TransactionListener {
 
+    private final ExecutorService executorService;
 
     @KafkaListener(topics = "transactions", id = "tnxListener")
     public void listen(Transaction transaction) {
-        System.out.println(transaction);
+        log.debug("Received transaction " + transaction.getId() + " for processing.");
+        Transaction result = executorService.process(transaction);
     }
 }
--- src/main/java/com/nextfin/executor/service/impl/ExecutorServiceImpl.java
+++ src/main/java/com/nextfin/executor/service/impl/ExecutorServiceImpl.java
@@ -2,6 +2,11 @@ package com.nextfin.executor.service.impl;
 
 import com.nextfin.executor.service.ExecutorService;
 import com.nextfin.transaction.entity.Transaction;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 
 public class ExecutorServiceImpl implements ExecutorService {
     @Override
